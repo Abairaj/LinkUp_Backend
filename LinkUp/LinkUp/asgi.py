87ee8ff7621c2e7d89738729eada1bde2847bcbe
Abairaj <abairajpoinachi@gmail.com>
2023-06-24 10:02:25 +0530

    daphne error 5

--- LinkUp/LinkUp/asgi.py
+++ LinkUp/LinkUp/asgi.py
@@ -1,12 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter,get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import users.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LinkUp.settings')
-django_asgi_app = get_default_application()
-# get_asgi_application()
+django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter({
